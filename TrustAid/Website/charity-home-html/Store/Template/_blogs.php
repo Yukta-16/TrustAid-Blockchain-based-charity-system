d3f497eeeef53385ac95d8cfa8eabd613590686e
Yukta-16 <71117539+Yukta-16@users.noreply.github.com>
2022-01-18 20:27:25 +0530

    store added

---
+++ TrustAid/Website/charity-home-html/Store/Template/_blogs.php
@@ -0,0 +1,30 @@
+
+<section id="blogs">
+    <div class="container py-4">
+        <hr>
+
+        <div class="owl-carousel owl-theme">
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog1.jpg" alt="cart image" class="card-img-top">
+                    <a href="#" class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog2.jpg" alt="cart image" class="card-img-top">
+                    <a href="#" class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="card border-0 font-rale mr-5" style="width: 18rem;">
+                    <h5 class="card-title font-size-16">Upcoming Mobiles</h5>
+                    <img src="./assets/blog/blog3.jpg" alt="cart image" class="card-img-top">
+                   <a href="#" class="color-second text-left">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
